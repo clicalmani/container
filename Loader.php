aedd719cb65db3904dd1775b61b1bbf70287c4d6
honorable85 <honorable85@gmail.com>
2023-09-24 18:23:45 +0100

    First commit

---
+++ Loader.php
@@ -0,0 +1,54 @@
+<?php
+namespace Clicalmani\Container;
+
+global $root_path;
+
+class SPL_Loader 
+{
+    private $bindings = [
+        'App/' => 'app/',
+        'Http/' => 'http/',
+        'Models/' => 'models/',
+        'Middlewares/' => 'middlewares/',
+        'Controllers/' => 'controllers/',
+        'Authenticate/' => 'authenticate/'
+    ];
+
+    public function __construct(private $root_path)
+    {
+        global $root_path;
+
+        $root_path = $this->root_path;
+        spl_autoload_register(fn($className) => $this->load($className)); // Registers the autoloader
+    }
+
+    public function load(string $className)
+    {
+        $fileName  = '';
+        $namespace = '';
+        
+        if (false !== ($lastNsPos = strripos($className, '\\'))) {
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+
+        $fileName = str_replace('\\', '/', $fileName);
+        $fileName .= $className . '.php';
+        
+        $fullFileName = $this->bind($this->root_path . '/' . $fileName);
+        
+        if ( file_exists($fullFileName) ) {
+            require_once $fullFileName;
+        }
+    }
+
+    public function bind($fullFileName)
+    {
+        foreach ($this->bindings as $key => $value) {
+            $fullFileName = str_replace($key, $value, $fullFileName);
+        }
+
+        return $fullFileName;
+    }
+}
